db653a4276f4cf17c3addbdd678b0a28403a2e79
Herick Alexandre <herick386@gmail.com>
2026-01-09 13:28:12 -0400

    adicionando pacote de imagens, ajustando layout, funcionalidade e componentização

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
+import { Container } from "./components/Container";
+import { Heading } from "./components/Heading";
+
 import "./styles/global.css";
 import "./styles/theme.css";
-import { Heading } from "./components/Heading";
 
 export function App() {
   return (
-    <div>
-      <Heading>Projeto Chronos Pomodoro! 🍅⏲️</Heading>
-      <p>Tá na hora de se dedicar para valer meu véioooo</p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio, alias
-        ea. Ex dolores corrupti minus sit quibusdam! Assumenda saepe omnis
-        magnam! Magni deserunt amet nulla aperiam repellendus optio laboriosam
-        itaque.
-      </p>
-      <Heading>Alguma outra coisa e tal lol</Heading>
-      <p>enuenneodneniniodneio</p>
-    </div>
+    <>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
+    </>
   );
 }
