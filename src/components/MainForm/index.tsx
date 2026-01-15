30f760d36e73f01a18a6bd986b27d593215b44d4
Herick Alexandre <herick386@gmail.com>
2026-01-14 21:08:45 -0400

    alteração na lógica do form, mas ainda incompleta

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,16 +2,25 @@ import { PlayIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
+import { useState } from 'react';
 
 export function MainForm() {
+  const [taskName, setTaskName] = useState('');
+
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  }
+
   return (
-    <form className='form' action=''>
+    <form onSubmit={handleCreateNewTask} className='form' action=''>
       <div className='formRow'>
         <DefaultInput
           id='task'
           placeholder='Dê um nome para a sua tarefa!'
           labelText='Escreva a sua tarefa:'
           required
+          value={taskName}
+          onChange={e => setTaskName(e.target.value)}
         />
       </div>
       <div className='formRow'>
