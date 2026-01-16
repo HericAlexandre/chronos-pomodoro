a84998dac07844e6cad68f5b051b656aac75e22b
Herick Alexandre <herick386@gmail.com>
2026-01-15 23:43:58 -0400

    modificações no form, dando inicio na mudança dos dados. Ainda resta lógica para ser trabalhada

--- src/components/DefaultInput/index.tsx
+++ src/components/DefaultInput/index.tsx
@@ -1,27 +1,29 @@
-import styles from "./styles.module.css";
+import React from 'react';
+import styles from './styles.module.css';
 
 type DefaultInputProps = {
   placeholder: string;
   id: string;
   labelText?: string;
-} & React.ComponentProps<"input">;
+} & React.ComponentProps<'input'>;
 
-export function DefaultInput({
-  placeholder,
-  id,
-  labelText,
-  ...rest
-}: DefaultInputProps) {
+export const DefaultInput = React.forwardRef<
+  HTMLInputElement,
+  DefaultInputProps
+>(function DefaultInput({ placeholder, id, labelText, ...rest }, ref) {
   return (
     <div className={styles.field}>
       {labelText && <label htmlFor={id}>{labelText}</label>}
       <input
+        ref={ref}
         className={styles.inputPadrao}
         id={id}
-        type="text"
+        type='text'
         placeholder={placeholder}
         {...rest}
       />
     </div>
   );
-}
+});
+
+DefaultInput.displayName = 'DefaultInput';
--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -3,12 +3,47 @@ import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
 import { useRef } from 'react';
+import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContex';
 
 export function MainForm() {
+  const { setState } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'workTime',
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: 1,
+        secondsRemaining,
+        formattedSecondsRemaining: '00:00',
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
   }
 
   return (
@@ -20,6 +55,11 @@ export function MainForm() {
           labelText='Escreva a sua tarefa:'
           required
           ref={taskNameInput}
+          onInvalid={() =>
+            alert(
+              'Ocorreu um erro ao carregar sua tarefa. Verifique se o campo foi preenchido corretamente.',
+            )
+          }
         />
       </div>
       <div className='formRow'>
@@ -28,7 +68,7 @@ export function MainForm() {
       <div className='formRow'>
         <Cycles />
       </div>
-      <DefaultButton icon={<PlayIcon />} />
+      <DefaultButton type='submit' icon={<PlayIcon />} />
     </form>
   );
 }
